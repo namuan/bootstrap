108da92e7601cf8bd171738a38760168d8816f37
namuan <nleghari@gmail.com>
2011-09-01 23:48:38 +0000

    added script to setup and run mongodb server on 64bit linux

---
+++ setup/setup_mongodb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+INSTALL_DIR=/mnt/setup
+WORK_HOME=/mnt/local
+
+cd $INSTALL_DIR
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-1.8.3.tgz
+tar xf mongodb-linux-x86_64-1.8.3.tgz
+mv mongodb-linux-x86_64-1.8.3 $WORK_HOME/mongodb
+echo 'export PATH='$WORK_HOME'/mongodb/bin:$PATH' >> ~/.devprofile
+source ~/.devprofile
+
+mkdir $WORK_HOME/data
+mongod --dbpath $WORK_HOME/data
