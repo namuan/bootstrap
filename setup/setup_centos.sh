ba83c8897708bef8a87fdf6f066c7e71fda7ddb3
namuan <nleghari@gmail.com>
2011-09-25 19:19:41 +0200

    not installing git from source

--- setup/setup_centos.sh
+++ setup/setup_centos.sh
@@ -33,22 +33,7 @@ rm -rf /mnt/*
 mkdir -vp /mnt/setup;mkdir -vp /mnt/softwares;mkdir -vp /mnt/workspace;mkdir -vp /mnt/.m2;mkdir -vp /mnt/.ivy2;mkdir -vp /mnt/local
 sudo chown -R imon:imon /mnt
 
-# setup git
-setupgit() {
-
-        cd /mnt/setup
-        wget http://kernel.org/pub/software/scm/git/git-1.7.3.5.tar.bz2
-        tar xf git-1.7.3.5.tar.bz2
-        cd git-1.7.3.5
-        ./configure --prefix=/mnt/local/git --without-tcltk
-        make
-        make install
-
-}
-
-#export -f setupgit
-#su imon -c "setupgit"
-yum install git-all -y
+yum install git -y
 
 setupnode() {
         source ~/.devprofile
